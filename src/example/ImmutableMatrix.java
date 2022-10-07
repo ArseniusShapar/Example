ed6ec9f1a1ca96da5e93d5630eac984cfea2ea14
Arseniy <arsenshapar@gmail.com>
2022-10-07 17:11:11 +0300

    commit

--- src/example/ImmutableMatrix.java
+++ src/example/ImmutableMatrix.java
@@ -13,22 +13,11 @@ public class ImmutableMatrix extends Matrix {
         super(matrix.getArray());
     }
 
+    @Override
     public ImmutableMatrix toLowerTriangle() {
         MatrixMathOperations matrixMathOperations = new MatrixMathOperations();
         Matrix triangleMatrix = matrixMathOperations.toLowerTriangle(this);
         return new ImmutableMatrix(triangleMatrix);
-    }
-
-    public void print() {
-        int height = super.getHeight();
-        int width = super.getWidth();
-        double[][] array = super.getArray();
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                System.out.print(array[i][j] + " ");
-            }
-            System.out.print("\n");
-        }
     }
 
 }
--- src/example/Main.java
+++ src/example/Main.java
@@ -3,12 +3,17 @@ package example;
 public class Main {
     public static void main(String[] args) {
         double[][] array = new double[][] {
-                {1.2, 2.7},
-                {-2.0, 1.3}};
+                {1, 2},
+                {4, 2}};
         Matrix matrix = new Matrix(array);
         ImmutableMatrix immutableMatrix = new ImmutableMatrix(array);
+
         matrix.toLowerTriangle().print();
+        System.out.println();
+
         immutableMatrix.toLowerTriangle().print();
+        System.out.println();
+
         immutableMatrix.print();
 
     }
